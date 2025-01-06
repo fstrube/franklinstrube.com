9cdd4a081e19f974d13b8ecac4abc56cb6d7c241
Franklin Strube <307318+fstrube@users.noreply.github.com>
2025-01-05 19:04:54 -0500

    added jquery and header/footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+$(function() {
+    $("#header .toggle-navigation").click(function(el) {
+        if ( $("#header .navigation:visible").length ) {
+            //var paddingTop = parseInt($(".main").css("paddingTop")) - $("#header .navigation").height();
+            //console.log('closing', paddingTop);
+            //$(".main").animate({"padding-top": paddingTop}, "fast");
+            $("#header .navigation").slideUp("fast", function() {$("#header .navigation").removeAttr("style");});
+        } else {
+            //var paddingTop = parseInt($(".main").css("paddingTop")) + $("#header .navigation").height();
+            //console.log('opening', paddingTop);
+            $("#header .navigation").slideDown("fast");
+            //$(".main").animate({"padding-top": paddingTop}, "fast");
+        }
+
+        return false;
+    });
+});
